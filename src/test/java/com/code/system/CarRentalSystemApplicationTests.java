2f76004038a9886fe2a29c8f4dce1f1bac8c28a6
Петрушина Полина <polina.petrushina@gazprombank.ru>
2024-09-27 12:05:16 +0300

    git init

---
+++ src/test/java/com/code/system/CarRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
